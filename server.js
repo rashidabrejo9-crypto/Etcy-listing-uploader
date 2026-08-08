aaff2dd838afe10a56f9366745c5a11c0de5b1a5
rashidabrejo9-crypto <rashidabrejo9@gmail.com>
2026-08-08 21:31:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3003;
-const PUBLIC_DIR = path.join(process.cwd(), "public");
+const PUBLIC_DIR = process.cwd();
 const upload = multer({ dest: path.join(process.cwd(), "tmp") });
 
 app.use(express.json({ limit: "2mb" }));
